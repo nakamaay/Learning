d4f48e941c90d74be7452d94e68dfc7bb406d72b
naka <pcyama7ka@gmail.com>
2020-08-16 13:58:27 +0900

    学習ファイルをファイルを解凍するプログラムを追加した

--- Memorization.py
+++ Memorization.py
@@ -5,6 +5,8 @@ import tkinter.ttk as ttk
 from tkinter import filedialog
 import random
 import os
+import zipfile
+
 
 # メイン画面表示
 root = tk.Tk()
@@ -12,9 +14,9 @@ root.title("top")
 root.geometry("600x600")
 
 # 問題文ファイルの読み込み
-path = '問題/問題文.txt'
-with open(path, encoding="utf-8") as f:
-    l_strip = [s.strip() for s in f.readlines()]
+# path = '問題/問題文.txt'
+# with open(path, encoding="utf-8") as f:
+#     l_strip = [s.strip() for s in f.readlines()]
 
 
 
@@ -22,17 +24,38 @@ with open(path, encoding="utf-8") as f:
 
 # 学習セット読み込み画面
 def  Learning_set_Read(event):
-    fTyp = [("","zip")]
+    fTyp = [("",".zip")]
     iDir = os.path.abspath(os.path.dirname(__file__))
-    filepath = filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
-    print(filepath)
+    filepath = filedialog.askopenfilename(filetypes = fTyp, initialdir = iDir)
+    with open("設定.txt", mode='w') as f:
+        f.write(filepath)
 
 # 問題画面
 def problem(event):
     global r
-    global txt
+
 
     # 問題画面表示
+    with open("設定.txt") as f:
+        zipfilepath = f.read()
+
+
+    # ファイルを解凍する
+    with zipfile.ZipFile(zipfilepath) as zf:
+        zf.extractall()
+
+    
+    basename = os.path.basename(zipfilepath)
+    Folder_name = basename.replace('.zip', '')
+
+
+    with open(Folder_name+"/config.txt") as f:
+        l = [s.strip() for s in f.readlines()]
+        print(l)
+
+
+
+
     problem1 = tk.Tk()
     problem1.title("problem")
     problem1.geometry("600x600")
@@ -46,7 +69,56 @@ def problem(event):
 
 
 
+
 """
+def Question_creation(event):
+    Question_creation1 = tk.Tk()
+    Question_creation1.title("Question_creation")
+    Question_creation1.geometry("600x600")
+
+
+    # 問題文
+    lbl = tk.Label(Question_creation1, text='問題文', font=("",20))
+    lbl.place(x=200,y=50)
+
+    txt = tk.Entry(Question_creation1,width=20)
+    txt.place(x=200,y=150)
+
+
+
+    Question1_lbl = tk.Label(Question_creation1, text='選択肢１',font=("",20))
+    Question1_lbl.place(x=50,y=250)
+
+
+    Question1_txt = tk.Entry(Question_creation1, width=20)
+    Question1_txt.place(x=50,y=300)
+
+
+    Question2_lbl = tk.Label(Question_creation1, text='選択肢2',font=("",20))
+    Question2_lbl.place(x=200,y=250)
+
+
+    Question2_txt = tk.Entry(Question_creation1, width=20)
+    Question2_txt.place(x=200,y=300)
+
+    Question3_lbl = tk.Label(Question_creation1, text='選択肢3',font=("",20))
+    Question3_lbl.place(x=350,y=250)
+
+
+    Question3_txt = tk.Entry(Question_creation1, width=20)
+    Question3_txt.place(x=350,y=300)
+
+    Question4_lbl = tk.Label(Question_creation1, text='説明文',font=("",20))
+    Question4_lbl.place(x=200,y=350)
+
+
+    Question4_txt = tk.Entry(Question_creation1, width=20)
+    Question4_txt.place(x=200,y=400)
+
+
+
+
+
 # 正解不正解判定
 def Answer(event):
     global txt
@@ -59,13 +131,22 @@ def Answer(event):
 """
 
 
-# 開始ボタン表示
+# トップ画面のボタン
+
+# 開始
 Button = tk.Button(text='暗記開始',font=("",30))
-Button.place(x=250,y=150)
+Button.place(x=200,y=150)
 Button.bind("<Button-1>",problem)
 
 # 学習セットの読み込み
 Learning_set_Read_Button = tk.Button(text='学習セット読み込み',font=("",30))
-Learning_set_Read_Button.place(x=250,y=200)
+Learning_set_Read_Button.place(x=200,y=250)
 Learning_set_Read_Button.bind("<Button-1>",Learning_set_Read)
+
+# 問題作成
+# Question_creation_Button = tk.Button(text='問題作成',font=("",30))
+# Question_creation_Button.place(x=200,y=350)
+# Question_creation_Button.bind("<Button-1>",Question_creation)
+
+
 root.mainloop()
