d8dc2f2403f2b4638a7fdcebb41d3e5a108d3a54
naka <pcyama7ka@gmail.com>
2020-08-21 19:56:28 +0900

    問題を何回もできるようした

--- Memorization.py
+++ Memorization.py
@@ -1,23 +1,19 @@
 # モジュールのインポート
 import tkinter as tk
-import tkinter.messagebox as tkmsg
+import tkinter.messagebox as messagebox
 import tkinter.ttk as ttk
 from tkinter import filedialog
 import random
 import os
 import zipfile
-
+a = 0
+b = 0
 
 # メイン画面表示
 root = tk.Tk()
 root.title("top")
-root.geometry("600x600")
-
-# 問題文ファイルの読み込み
-# path = '問題/問題文.txt'
-# with open(path, encoding="utf-8") as f:
-#     l_strip = [s.strip() for s in f.readlines()]
-
+root.minsize(width=600, height=600)
+root.maxsize(width=600, height=600)
 
 
 
@@ -29,6 +25,29 @@ def  Learning_set_Read(event):
     filepath = filedialog.askopenfilename(filetypes = fTyp, initialdir = iDir)
     with open("設定.txt", mode='w') as f:
         f.write(filepath)
+
+
+
+
+def One_choice(event):
+    global a
+    a = 1
+
+
+def Two_choices(event):
+    global a
+    a = 2
+
+
+def Three_choices(event):
+    global a
+    a = 3
+
+
+
+
+
+
 
 # 問題画面
 def problem(event):
@@ -48,9 +67,8 @@ def problem(event):
     Folder_name = basename.replace('.zip', '')
 
 
-    files = os.listdir(Folder_name)  
+    files = os.listdir(Folder_name)
     count = len(files)/4
-    print(count)
     r = random.randint(1, count)
 
 
@@ -63,90 +81,63 @@ def problem(event):
     with open(Folder_name+"/"+str(r)+"-1.txt") as f:
         lbl =  tk.Label(problem1, text=f.read(), font =("",20))
         lbl.place(x=200,y=50)
- 
-
-
-        with open(Folder_name+"/"+str(r)+"-2.txt") as f:
-            l_strip = [s.strip() for s in f.readlines()]
-            print(l_strip)
-
-
-            One_choice_button = tk.Button(problem1, text=l_strip[0], font=("",20))
-            One_choice_button.place(x=50,y=100)
-            # One_choice_button.bind("<Button-1>",)
-
-            Two_choices_button = tk.Button(problem1, text=l_strip[1], font=("",20))
-            Two_choices_button.place(x=100,y=100)
-
-            Three_choices_button = tk.Button(problem1, text=l_strip[2], font=("",20))
-            Three_choices_button.place(x=150,y=100)
-
-# Learning_set_Read_Button.bind("<Button-1>",Learning_set_Read)
-
-
-"""
-def Question_creation(event):
-    Question_creation1 = tk.Tk()
-    Question_creation1.title("Question_creation")
-    Question_creation1.geometry("600x600")
-
-
-    # 問題文
-    lbl = tk.Label(Question_creation1, text='問題文', font=("",20))
-    lbl.place(x=200,y=50)
-
-    txt = tk.Entry(Question_creation1,width=20)
-    txt.place(x=200,y=150)
 
 
 
-    Question1_lbl = tk.Label(Question_creation1, text='選択肢１',font=("",20))
-    Question1_lbl.place(x=50,y=250)
-
-
-    Question1_txt = tk.Entry(Question_creation1, width=20)
-    Question1_txt.place(x=50,y=300)
-
-
-    Question2_lbl = tk.Label(Question_creation1, text='選択肢2',font=("",20))
-    Question2_lbl.place(x=200,y=250)
-
-
-    Question2_txt = tk.Entry(Question_creation1, width=20)
-    Question2_txt.place(x=200,y=300)
-
-    Question3_lbl = tk.Label(Question_creation1, text='選択肢3',font=("",20))
-    Question3_lbl.place(x=350,y=250)
-
-
-    Question3_txt = tk.Entry(Question_creation1, width=20)
-    Question3_txt.place(x=350,y=300)
-
-    Question4_lbl = tk.Label(Question_creation1, text='説明文',font=("",20))
-    Question4_lbl.place(x=200,y=350)
-
-
-    Question4_txt = tk.Entry(Question_creation1, width=20)
-    Question4_txt.place(x=200,y=400)
+    with open(Folder_name+"/"+str(r)+"-2.txt") as f:
+        l_strip = [s.strip() for s in f.readlines()]
+        print(l_strip)
 
 
 
 
+    with open(Folder_name+"/"+str(r)+"-3.txt") as f:
+        lbl =  tk.Label(problem1, text=f.read(), font =("",20))
+        lbl.place(x=200,y=100)
 
-# 正解不正解判定
-def Answer(event):
-    global txt
-    global r
-    tmp = txt.get()
-    if tmp == l_strip1[r]:
-        tkmsg.showinfo(title="正解", message="正解")
-    else:
-        tkmsg.showinfo(title="不正解", message="不正解答えは"+l_strip1[r])
-"""
+
+    with open(Folder_name+"/"+str(r)+"-4.txt") as f:
+        global b
+        b=f.read()
+
+    One_choice_button = tk.Button(problem1, text=l_strip[0], font=("",20))
+    One_choice_button.place(x=50,y=150)
+    One_choice_button.bind("<Button-1>", One_choice)
+
+
+    Two_choices_button = tk.Button(problem1, text=l_strip[1], font=("",20))
+    Two_choices_button.place(x=100,y=150)
+    Two_choices_button.bind("<Button-1>", Two_choices)
+
+
+    Three_choices_button = tk.Button(problem1, text=l_strip[2], font=("",20))
+    Three_choices_button.place(x=150,y=150)
+    Three_choices_button.bind("<Button-1>", Three_choices)
+
+
+
+
+    def Answer(event):
+        if (a == int(b)):
+            res = messagebox.showinfo("正解", "正解")
+            if res == "ok":
+                button = tk.Button(problem1, text='次の問題へ', font=("",20))
+                button.place(x=200,y=400)
+                button.bind("<Button-1>",problem)
+
+        else:
+            res = messagebox.showinfo("不正解", "不正解:"+l_strip[int(b)-1])
+            if res == "ok":
+                button = tk.Button(problem1, text='次の問題へ', font=("",20))
+                button.place(x=200,y=400)
+                button.bind("<Button-1>",problem)
+
+    button = tk.Button(problem1, text='回答', font=("",20))
+    button.place(x=200, y=300)
+    button.bind("<Button-1>", Answer)
 
 
 # トップ画面のボタン
-
 # 開始
 Button = tk.Button(text='暗記開始',font=("",30))
 Button.place(x=200,y=150)
@@ -156,11 +147,6 @@ Button.bind("<Button-1>",problem)
 Learning_set_Read_Button = tk.Button(text='学習セット読み込み',font=("",30))
 Learning_set_Read_Button.place(x=200,y=250)
 Learning_set_Read_Button.bind("<Button-1>",Learning_set_Read)
-
-# 問題作成
-# Question_creation_Button = tk.Button(text='問題作成',font=("",30))
-# Question_creation_Button.place(x=200,y=350)
-# Question_creation_Button.bind("<Button-1>",Question_creation)
 
 
 root.mainloop()
